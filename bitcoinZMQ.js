9c43887fbcc72324caa2146dfb52d54d85c4e066
root <korybnewton@gmail.com>
2019-12-04 01:01:40 +0000

    remove address

--- bitcoinZMQ.js
+++ bitcoinZMQ.js
@@ -4,7 +4,7 @@ const activeClients = require('./webSocketServer').clients;
 async function zmqSubscription() {
   const sock = new zmq.Subscriber();
 
-  sock.connect('tcp://167.172.134.157:29000');
+  sock.connect('tcp://127.0.0.1:29000');
   sock.subscribe('rawblock');
   console.log('bitcoin zmq subscriber connected to port 29000');
 
